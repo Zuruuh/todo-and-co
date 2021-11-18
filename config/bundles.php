f4d58d2412f1a9ba7c9a64cb20e9493df127eba5
Younès Ziadi <ziadi.mail.pro@gmail.com>
2021-11-18 14:20:56 +0100

    ✨ Added Webpack Encore Bundle to bundle loader

--- config/bundles.php
+++ config/bundles.php
@@ -5,4 +5,5 @@ return [
     Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
