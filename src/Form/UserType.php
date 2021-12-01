739c2815b03ae2917ba3d1c429f3a0ded79ab75a
Younès Ziadi <ziadi.mail.pro@gmail.com>
2021-12-01 07:40:13 +0100

    ✅ Ignored forms from test coverage

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -9,6 +9,9 @@ use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 
+/**
+ * @codeCoverageIgnore
+ */
 class UserType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
