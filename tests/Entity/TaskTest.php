622ce71ebdd23ea3f9dd98ec45b3ea32f578b7ea
Younès Ziadi <ziadi.mail.pro@gmail.com>
2021-12-01 07:35:54 +0100

    ✅ Created tests for app entities

---
+++ tests/Entity/TaskTest.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Tests\Entity;
+
+use App\Entity\Task;
+use PHPUnit\Framework\TestCase;
+
+class TaskTest extends TestCase
+{
+    public const TITLE   = 'Task Title';
+    public const CONTENT = 'Task Content';
+
+    public function testId(): void
+    {
+        $task = new Task();
+        $this->assertNull($task->getId());
+    }
+
+    public function testTitle(): void
+    {
+        $task = new Task();
+        $this->assertNull($task->getTitle());
+
+        $task->setTitle(self::TITLE);
+        $this->assertEquals(self::TITLE, $task->getTitle());
+    }
+
+    public function testContent(): void
+    {
+        $task = new Task();
+        $this->assertNull($task->getContent());
+
+        $task->setContent(self::CONTENT);
+        $this->assertEquals(self::CONTENT, $task->getContent());
+    }
+
+    public function testIsDone(): void
+    {
+        $task = new Task();
+        $this->assertFalse($task->getIsDone());
+
+        $task->toggle();
+        $this->assertTrue($task->getIsDone());
+
+        $task->setIsDone(false);
+        $this->assertFalse($task->getIsDone());
+    }
+
+    public function testCreatedAt(): void
+    {
+        $task = new Task();
+        $date = new \Datetime();
+        $task->setCreatedAt($date);
+
+        $this->assertEquals($date, $task->getCreatedAt());
+    }
+}
---
+++ tests/Entity/UserTest.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Tests\Entity;
+
+use App\Entity\User;
+use PHPUnit\Framework\TestCase;
+
+class UserTest extends TestCase
+{
+    public const USERNAME = 'User Username';
+    public const EMAIL    = 'user@email.com';
+    public const PASSWORD = 'p455w0rd';
+
+    public const ROLE_1 = 'ROLE_REDACTOR';
+    public const ROLE_2 = 'ROLE_ADMIN';
+
+    public function testId(): void
+    {
+        $user = new User();
+        $this->assertNull($user->getId());
+    }
+
+    public function testUsername(): void
+    {
+        $user = new User();
+        $this->assertEmpty($user->getUserIdentifier());
+
+        $user->setUsername(self::USERNAME);
+
+        $this->assertEquals(self::USERNAME, $user->getUserIdentifier());
+    }
+
+    public function testEmail(): void
+    {
+        $user = new User();
+        $this->assertNull($user->getEmail());
+
+        $user->setEmail(self::EMAIL);
+        $this->assertEquals(self::EMAIL, $user->getEmail());
+    }
+
+    public function testPassword(): void
+    {
+        $user = new User();
+        $this->assertNull($user->getPassword());
+
+        $user->setPassword(self::PASSWORD);
+        $this->assertEquals(self::PASSWORD, $user->getPassword());
+    }
+
+    public function testRoles(): void
+    {
+        $user = new User();
+        $this->assertContains(User::USER_ROLE, $user->getRoles());
+
+        $user->setRoles([self::ROLE_1, self::ROLE_1]);
+        $roles = $user->getRoles();
+
+        $this->assertContainsOnly('string', $roles);
+        $this->assertTrue(count($roles) === 2);
+
+        $this->assertContains(User::USER_ROLE, $roles);
+        $this->assertContains(self::ROLE_1, $roles);
+        $this->assertNotContains(self::ROLE_2, $roles);
+
+        $roles = $user->addRole(self::ROLE_2)->getRoles();
+
+        $this->assertContains(User::USER_ROLE, $roles);
+        $this->assertContains(self::ROLE_1, $roles);
+        $this->assertContains(self::ROLE_2, $roles);
+
+        $roles = $user->setRoles([])->getRoles();
+
+        $this->assertContains(User::USER_ROLE, $roles);
+        $this->assertNotContains(self::ROLE_1, $roles);
+        $this->assertNotContains(self::ROLE_2, $roles);
+
+        $roles = $user->addRole([self::ROLE_1, self::ROLE_2])->getRoles();
+
+        $this->assertContains(User::USER_ROLE, $roles);
+        $this->assertContains(self::ROLE_1, $roles);
+        $this->assertContains(self::ROLE_2, $roles);
+    }
+}
